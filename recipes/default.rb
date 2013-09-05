896879d3e86ffc9e056e123b1462457c2d21c91e
Wil Reichert <wil.reichert@kt.com>
2013-09-05 13:43:04 +0900

    test!

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,10 +7,11 @@ class Chef::Recipe
   include KTCUtils
 end
 
-d1 = get_openstack_service_template(get_interface_address("management"), "5000")
+iface = get_interface "management"
+d1 = get_openstack_service_template(iface, "5000")
 register_member("identity-api", d1)
 
-d2 = get_openstack_service_template(get_interface_address("management"), "35357")
+d2 = get_openstack_service_template(iface, "35357")
 register_member("identity-admin", d2)
 
 set_rabbit_servers "identity"
@@ -19,7 +20,7 @@ set_database_servers "identity"
 set_service_endpoint "identity-api"
 set_service_endpoint "identity-admin"
 
-node.default["openstack"]["identity"]["bind_interface"] = get_interface "management"
+node.default["openstack"]["identity"]["bind_interface"] = iface
 
 include_recipe "openstack-common"
 include_recipe "openstack-common::logging"
