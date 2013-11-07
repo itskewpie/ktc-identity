b6b141919ac2c97f7c6fa4d5d2da247baa3ecfba
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-11-08 05:09:02 +0900

    auto_version to 1.1.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "wil.reichert@kt.com"
 license           "All rights reserved"
 description	      "Installs/Configures Openstack Identity Service"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.2'
+version '1.1.3'
 
 %w{ centos ubuntu }.each do |os|
   supports os
